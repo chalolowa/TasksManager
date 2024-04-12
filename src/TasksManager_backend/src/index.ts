19ff48bf7241b64274e70382d5ee645901a61c18
chalolowa <wangwelocha@students.uonbi.ac.ke>
2024-04-12 15:57:13 +0300

    On branch master  Changes to be committed: 	new file:   package-lock.json 	modified:   package.json 	modified:   src/TasksManager_backend/src/index.ts 	modified:   tsconfig.json

--- src/TasksManager_backend/src/index.ts
+++ src/TasksManager_backend/src/index.ts
@@ -1,7 +1,70 @@
-import { Canister, query, text } from 'azle';
+import { Canister, ic, query, StableBTreeMap, text } from 'azle';
+import express from 'express';
+import http from 'http';
+import { uuid } from 'uuidv4';
+import cors from 'cors';
 
-export default Canister({
-    greet: query([text], text, (name) => {
-        return `Hello, ${name}!`;
-    })
+interface Task {
+    id: string,
+    title: string,
+    description: string,
+    status: 'pending' | 'in-progress' | 'complete',
+    createdAt: Date,
+    updatedAt: Date | null
+}
+
+const TasksStorage = StableBTreeMap<string, Task>(0);
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.json(TasksStorage.values());
+});
+
+app.get('/tasks/:id', (req, res) => {
+    const TaskId = req.params.id;
+    const retrieved = TasksStorage.get(TaskId);
+    res.json(retrieved).status(200);
+});
+
+app.post('/tasks/new', (req, res) => {
+    const newTask: Task = {id: uuid(), createdAt: getCurrentDate(), ...req.body};
+    TasksStorage.insert(newTask.id, newTask);
+    res.json(newTask).status(201);
+});
+
+app.put('/tasks/:id', (req, res) => {
+    const TaskId = req.params.id;
+    const RetrievedTaskId = TasksStorage.get(TaskId);
+    if ("None" in RetrievedTaskId) {
+        res.status(400).send('There is no task with the provided id ${TaskId}!!! The task does not exist')
+    } else {
+        const task = RetrievedTaskId.Some;
+        const updatedTask = {...task, ...req.body, updatedAt: getCurrentDate()};
+        TasksStorage.insert(task?.id, updatedTask);
+        res.json(updatedTask);
+    }
+});
+
+app.delete('/tasks/:id', (req, res) => {
+    const TaskId = req.params.id;
+    const RetrievedTaskId = TasksStorage.get(TaskId);
+    if ("None" in RetrievedTaskId) {
+        res.status(400).send('There is no task with the provided id ${TaskId}!!! The task does not exist')
+    } else {
+        TasksStorage.remove(TaskId);
+    }
+});
+
+const server = http.createServer(app);
+server.listen(3000, () => {
+    console.log('Server running successfully');
 })
+
+function getCurrentDate() {
+    const timestamp = new Number(ic.time());
+    return new Date(timestamp.valueOf() / 1000_000);
+ }
